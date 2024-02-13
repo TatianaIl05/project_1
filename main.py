f66bfc9d572df654ce1a12fc581c5858dafe8fc8
Tania <t.ilinykh1@g.nsu.ru>
2024-02-13 17:58:38 +0700

    final changes for functions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import turtle as t
+
+
 def rhombus(x, y, a):
     t.fillcolor('green')
     t.begin_fill()
@@ -6,15 +8,14 @@ def rhombus(x, y, a):
     t.up()
     t.setposition(x, y)
     t.down()
-    t.forward(a)
-    t.right(135)
-    t.forward(a)
-    t.right(45)
-    t.forward(a)
-    t.right(135)
-    t.forward(a)
-    t.right(45)
+    for i in range(2):
+        t.forward(a)
+        t.right(135)
+        t.forward(a)
+        t.right(45)
     t.end_fill()
+
+
 def rectangle(x, y, a, b):
     t.fillcolor('pink')
     t.begin_fill()
@@ -22,15 +23,14 @@ def rectangle(x, y, a, b):
     t.up()
     t.setposition(x, y)
     t.down()
-    t.forward(a)
-    t.right(90)
-    t.forward(b)
-    t.right(90)
-    t.forward(a)
-    t.right(90)
-    t.forward(b)
-    t.right(90)
+    for i in range(2):
+        t.forward(a)
+        t.right(90)
+        t.forward(b)
+        t.right(90)
     t.end_fill()
+
+
 def star(x, y, a):
     t.fillcolor('purple')
     t.begin_fill()
@@ -38,31 +38,34 @@ def star(x, y, a):
     t.up()
     t.setposition(x, y)
     t.down()
-    for i in range (5):
+    for i in range(5):
         t.forward(a)
         t.left(72)
         t.forward(a)
         t.right(144)
     t.end_fill()
-def square(x,y,a):
+
+
+def square(x, y, a):
     t.fillcolor('red')
     t.begin_fill()
     t.color('red')
     t.up()
-    t.setposition(x,y)
+    t.setposition(x, y)
     t.down()
     for i in range(4):
         t.forward(a)
         t.left(90)
     t.end_fill()
-def triangle(x,y,b):
+
+
+def triangle(x, y, b):
     import math
-    from math import sqrt
     t.fillcolor('yellow')
     t.begin_fill()
     t.color('yellow')
     t.up()
-    t.setposition(x,y)
+    t.setposition(x, y)
     t.down()
     t.forward(b)
     t.right(45)
@@ -71,7 +74,9 @@ def triangle(x,y,b):
     t.forward(b*math.sqrt(2))
     t.right(90)
     t.end_fill()
-def round(x, y, r, c):
+
+
+def circle(x, y, r, c):
     t.fillcolor(c)
     t.begin_fill()
     t.color(c)
@@ -80,6 +85,8 @@ def round(x, y, r, c):
     t.down()
     t.circle(r)
     t.end_fill()
+
+
 def trapeze(x, y, a, h):
     if a > 2 * h:
         t.fillcolor('blue')
